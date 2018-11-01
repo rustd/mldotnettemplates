be0a9353dbd19593fb81357e749a11cb72f10fc0
Pranav Rastogi <rustd@users.noreply.github.com>
2018-11-01 10:14:25 -0700

    Update to 0.6 Dynamic API

--- MLNETConsoleApp/MLNETConsoleApp/Program.cs
+++ MLNETConsoleApp/MLNETConsoleApp/Program.cs
@@ -3,16 +3,12 @@
  * found at https://aka.ms/mlnetsentimentanalysis, which provides a more detailed introduction to ML.NET and the scenario.*/
 
 using System;
+using System.IO;
+using Microsoft.ML.Runtime.Learners;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Core.Data;
 using Microsoft.ML;
-using Microsoft.ML.Data;
-using Microsoft.ML.Legacy;
-using Microsoft.ML.Legacy.Data;
-using Microsoft.ML.Legacy.Models;
-using Microsoft.ML.Legacy.Trainers;
-using Microsoft.ML.Legacy.Transforms;
 using Microsoft.ML.Runtime.Api;
-using Microsoft.ML.Trainers;
-using Microsoft.ML.Transforms;
 
 namespace MLNETConsoleApp
 {
@@ -20,42 +16,57 @@ namespace MLNETConsoleApp
     {
         static void Main()
         {
-
-            //1. Build an ML.NET pipeline for training a sentiment analysis model
+            // 1. Build an ML.NET pipeline for training a sentiment analysis model
             Console.WriteLine("Training a model for Sentiment Analysis using ML.NET");
-            var pipeline = new LearningPipeline
-            {
-                // 1a. Load the training data using a TextLoader.
-                new TextLoader(@"Data\wikipedia-detox-250-line-data.tsv").CreateFrom<SentimentData>(useHeader: true),
+            var env = new LocalEnvironment();
+            // 1a. Load the training data using a TextLoader.
+            var reader = new TextLoader(env,
+                                            new TextLoader.Arguments()
+                                            {
+                                                Separator = "tab",
+                                                HasHeader = true,
+                                                Column = new[]
+                                                {
+                                                    new TextLoader.Column("Label", DataKind.Bool, 0),
+                                                    new TextLoader.Column("Text", DataKind.Text, 1)
+                                                }
+                                            });
 
-                // 1b. Featurize the text into a numeric vector that can be used by the machine learning algorithm.
-                new TextFeaturizer("Features", "SentimentText"),
+            IDataView trainingDataView = reader.Read(new MultiFileSource(@"Data\wikipedia-detox-250-line-data.tsv"));
+            // 2. Create a pipeline to prepare your data, pick your features and apply a machine learning algorithm 
+            // 2a. Featurize the text into a numeric vector that can be used by the machine learning algorithm.
+            var pipeline = new TextTransform(env, "Text", "Features")  //Convert the text column to numeric vectors (Features column)   
+                                           .Append(new LinearClassificationTrainer(env, new LinearClassificationTrainer.Arguments(),
+                                                                                   "Features", "Label"));
 
-                // 1c. Add AveragedPerceptron (a linear learner) to the pipeline.
-                new AveragedPerceptronBinaryClassifier() { NumIterations = 10 }
-            };
+            // 2b. Add AveragedPerceptron (a linear learner) to the pipeline.
+            //new AveragedPerceptronBinaryClassifier() { NumIterations = 10 }
 
-            // 1d. Get a model by training the pipeline that was built.
-            PredictionModel<SentimentData, SentimentPrediction> model = pipeline.Train<SentimentData, SentimentPrediction>();
+            // 3. Get a model by training the pipeline that was built.
+            var model = pipeline.Fit(trainingDataView);
 
-            // 2. Evaluate the model to see how well it performs on different data (output the percent of examples classified correctly).
+            // 4. Evaluate the model to see how well it performs on different data (output the percent of examples classified correctly).
             Console.WriteLine("Training of model is complete \nTesting the model with test data");
-            var testData = new TextLoader(@"Data\wikipedia-detox-250-line-test.tsv").CreateFrom<SentimentData>(useHeader: true);
-            var evaluator = new BinaryClassificationEvaluator();
-            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
-            Console.WriteLine($"Accuracy of trained model for test data is: {metrics.Accuracy:P2}");
+            IDataView testDataView = reader.Read(new MultiFileSource(@"Data\wikipedia-detox-250-line-test.tsv"));
+            var predictions = model.Transform(testDataView);
+            var binClassificationCtx = new BinaryClassificationContext(env);
+            var metrics = binClassificationCtx.Evaluate(predictions, "Label");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
 
-            // 3. Save the model to file so it can be used in another app.
-            model.WriteAsync("sentiment_model.zip");
-
-            // 4. Use the model for a single prediction.
-            SentimentData testInput = new SentimentData { SentimentText = "ML.NET is fun, more samples at https://github.com/dotnet/machinelearning-samples" };
-            var sentiment = model.Predict(testInput).Sentiment ? "Positive" : "Negative";
+            // 5. Use the model for a single prediction.
+            var predictionFunct = model.MakePredictionFunction<SentimentData, SentimentPrediction>(env);
+            SentimentData testInput = new SentimentData { Text = "ML.NET is fun, more samples at https://github.com/dotnet/machinelearning-samples" };
+            var resultprediction = predictionFunct.Predict(testInput);
 
             /* This template uses a minimal dataset to build a sentiment analysis model which leads to relatively low accuracy. 
              * Building good Machine Learning models require large volumes of data. This template comes with a minimal dataset (Data/wikipedia-detox) for sentiment analysis. 
-             * In order to build a sentiment analysis model with higher accuracy please follow the walkthrough at https://aka.ms/mlnetsentimentanalysis*/
-            Console.WriteLine($"Predicted sentiment for \"{testInput.SentimentText}\" is: {sentiment}");
+             * In order to build a sentiment analysis model with higher accuracy please follow the walkthrough at https://aka.ms/mlnetsentimentanalysis/ */
+            Console.WriteLine($"Predicted sentiment for \"{testInput.Text}\" is: { (Convert.ToBoolean(resultprediction.Prediction) ? "Positive" : "Negative")}");
+
+            // 6. Save the model to file so it can be used in another app.
+            Console.WriteLine("Saving the model");
+            var fs = new FileStream("sentiment_model.zip", FileMode.Create, FileAccess.Write, FileShare.Write);
+            model.SaveTo(env, fs);
         }
 
         /// <summary>
@@ -63,10 +74,8 @@ namespace MLNETConsoleApp
         /// </summary>
         public class SentimentData
         {
-            [Column(ordinal: "0", name: "Label")]
-            public float Sentiment { get; set; }
-            [Column(ordinal: "1")]
-            public string SentimentText { get; set; }
+            public bool Label { get; set; }
+            public string Text { get; set; }
         }
 
         /// <summary>
@@ -75,7 +84,7 @@ namespace MLNETConsoleApp
         public class SentimentPrediction
         {
             [ColumnName("PredictedLabel")]
-            public bool Sentiment { get; set; }
+            public bool Prediction { get; set; }
         }
     }
 }
--- MLNETConsoleApp/MLNETModelLibrary/ModelPrediction.cs
+++ MLNETConsoleApp/MLNETModelLibrary/ModelPrediction.cs
@@ -3,9 +3,10 @@
  * found at https://aka.ms/mlnetsentimentanalysis, which provides a more detailed introduction to ML.NET and the scenario.
  * Note that this model was trained on a very small sample dataset, which leads to a relatively low accuracy.*/
 using System;
+using System.IO;
 using Microsoft.ML;
-using Microsoft.ML.Legacy;
 using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
 
 namespace MLNETModelLibrary
 {
@@ -14,11 +15,15 @@ namespace MLNETModelLibrary
         public static string Predict(SentimentData input)
         {
             // 1. Load the model from file.
-            var model = PredictionModel.ReadAsync<SentimentData, SentimentPrediction>(@"Models\sentiment_model.zip").Result;
+            var env = new LocalEnvironment();
+            var stream = new FileStream(@"Models\sentiment_model.zip", FileMode.Open, FileAccess.Read, FileShare.Read);
+            var model = TransformerChain.LoadFrom(env, stream);
 
-            // 2. Use the model for a single prediction.
-            var sentiment = model.Predict(input).Sentiment ? "Positive" : "Negative";
-            Console.WriteLine($"Predicted sentiment for \"{input.SentimentText}\" is: {sentiment}");
+            // 2. Predict the sentiment
+            var predictionFunct = model.MakePredictionFunction<SentimentData, SentimentPrediction>(env);
+            var resultprediction = predictionFunct.Predict(input);
+            var sentiment = (Convert.ToBoolean(resultprediction.Prediction) ? "Positive" : "Negative");
+            Console.WriteLine($"Predicted sentiment for \"{input.Text}\" is:" + sentiment);
             return sentiment;
         }
     }
@@ -28,10 +33,8 @@ namespace MLNETModelLibrary
     /// </summary>
     public class SentimentData
     {
-        [Column(ordinal: "0", name: "Label")]
-        public float Sentiment { get; set; }
-        [Column(ordinal: "1")]
-        public string SentimentText { get; set; }
+        public bool Label { get; set; }
+        public string Text { get; set; }
     }
 
     /// <summary>
@@ -40,6 +43,6 @@ namespace MLNETModelLibrary
     public class SentimentPrediction
     {
         [ColumnName("PredictedLabel")]
-        public bool Sentiment { get; set; }
+        public bool Prediction { get; set; }
     }
 }
